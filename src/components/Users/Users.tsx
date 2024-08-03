bff908a3056eefa379a6a8e5eb8fc7e1d6710e4f
Saket210 <saketgupta210@gmail.com>
2024-08-03 17:10:54 +0530

    scroll to location added and minor changes

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,13 +1,22 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Users.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { User } from "../../types/User";
 import { toast } from "react-toastify";
 
 const Users = ({activeUserId}:{activeUserId:number}) => {
   const [users, setUsers] = useState<Array<User>>([]);
   const [loading, setLoading] = useState<Boolean>(false);
+  const navigate = useNavigate();
+  const itemsRef = useRef<(HTMLDivElement | null)[]>([]);
+
+  useEffect(() => {
+    if (activeUserId) {
+      const tabIndex = users.findIndex((item) => item.id === activeUserId);
+      itemsRef.current[tabIndex]?.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "center" });
+    }
+  }, [activeUserId, users]);
 
   useEffect(() => {
     fetchUsers();
@@ -31,19 +40,20 @@ const Users = ({activeUserId}:{activeUserId:number}) => {
     <div className="users-container">
       <h1>Users</h1>
       <div className="cards-container">
-      {loading?<div className="loader"></div>:users.map((user) => {
+      {loading?<div className="loader"></div>:users.map((user, index) => {
         return (
-          <Link
-            to={`/users/${user.id}`}
+          <div
+            onClick={()=> navigate(`/users/${user.id}`)}
             key={user.id}
             className={activeUserId===user.id ? "user-card-div active" : "user-card-div"}
+            ref={(elementRef) => itemsRef.current[index] = elementRef}
           >
             <div className="user-avatar"><h1>{user.name.charAt(0)}</h1></div>
             <div className="card-details">
             <p>{user.name}</p>
             <p>{user.email}</p>
             </div>
-          </Link>
+          </div>
         );
       })}
       </div>
